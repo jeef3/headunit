3858d16436a5b65c99e336e47af6f3e83df30cb7
Jeff Knaggs <mail@jeef3.com>
2016-10-21 22:00:49 +1300

    Display screen resolution

--- src/main.py
+++ src/main.py
@@ -2,16 +2,19 @@ import time
 
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty, ObjectProperty
 from kivy.clock import Clock
+from kivy.core.window import Window
 
 class HeadunitWidget(Widget):
     time = ObjectProperty(None)
     date = ObjectProperty(None)
+    screen_resolution = StringProperty('');
 
     def update(self, dt):
         self.time = time.strftime('%H:%M')
         self.date = time.strftime('%A, %d %B')
+        self.screen_resolution = 'Screen: ' + str(Window.size)
 
 
 class HeadunitApp(App):
