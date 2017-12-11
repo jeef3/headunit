72a4d9683e977e1321710060e3b48e387e0a3671
Jeff Knaggs <mail@jeef3.com>
2017-12-11 22:39:03 +1300

    Add some movement

--- src/main.py
+++ src/main.py
@@ -1,28 +1,64 @@
 import time
 
 from kivy.app import App
+from kivy.animation import Animation
 from kivy.uix.widget import Widget
-from kivy.properties import StringProperty, ObjectProperty
+from kivy.properties import StringProperty, ObjectProperty, NumericProperty, BooleanProperty
 from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.core.image import Image as CoreImage
 from kivy.network.urlrequest import UrlRequest
+from kivy.metrics import dp
+from kivy.uix.stacklayout import StackLayout
 
 Window.size = (720, 480)
 
 key = 'cbb50b1f23a2230b10ccf6a3d8504fb0'
 
-class HeadunitWidget(Widget):
+class TimeAndDate(Widget):
     time = ObjectProperty(None)
     date = ObjectProperty(None)
-    # temp = StringProperty('')
+    greeting = StringProperty('صباح الخير')
+    scale = NumericProperty(1)
 
     def update(self, dt):
         self.time = time.strftime('%H:%M')
         self.date = time.strftime('%A, %d %B')
 
-    # def got_weather(self, request, data):
-    #     self.temp = str(data['main']['temp'])
+
+class HeadunitWidget(Widget):
+    anim = 0
+    greeting = StringProperty('صباح الخير')
+    lang = StringProperty('ar')
+    is_greeting_visible = BooleanProperty(False)
+
+    def update(self, dt):
+        self.time_and_date.update(dt)
+
+    def toggle(self, dt):
+        if (self.is_greeting_visible):
+            self.hide_greeting()
+            self.is_greeting_visible = False
+        else:
+            self.show_greeting()
+            self.is_greeting_visible = True
+
+
+    def hide_greeting(self):
+        if (self.anim):
+            self.anim.cancel(self.time_and_date)
+
+        self.anim = Animation(scale=1, duration=0.3, t='in_out_cubic')
+        self.anim &= Animation(y=0, duration=0.3, t='in_out_cubic')
+        self.anim.start(self.time_and_date)
+
+    def show_greeting(self):
+        if (self.anim):
+            self.anim.cancel(self.time_and_date)
+
+        self.anim = Animation(scale=0.6, duration=0.3, t='in_out_cubic')
+        self.anim &= Animation(y=dp(150), duration=0.3, t='in_out_cubic')
+        self.anim.start(self.time_and_date)
 
 
 class HeadunitApp(App):
@@ -30,10 +66,10 @@ class HeadunitApp(App):
     def build(self):
         app = HeadunitWidget()
 
-        # url = 'http://api.openweathermap.org/data/2.5/weather?id=2192362&units=metric&appid=' + key
-        # req = UrlRequest(url, on_success=app.got_weather)
-
         Clock.schedule_interval(app.update, 1.0 / 60.0)
+        Clock.schedule_once(app.toggle)
+        Clock.schedule_once(app.toggle, 10)
+
         return app
 
 
